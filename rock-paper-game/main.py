4db126b91802ec8baee5592eb5cafddcc217f4d3
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-04-05 09:00:17 +0300

    Update main.py

--- rock-paper-game/main.py
+++ rock-paper-game/main.py
@@ -10,7 +10,22 @@ def get_choices():
 
 def check_win(player,computer):
     #f string is a simpler way to combine strings together
-    print(f"You chose {player} computer chose {computer}" )
+    print(f"You chose {player}, computer chose {computer}" )
     if player == computer:
      return "It's a tie!"
+    elif player == "rock":
+     if computer == "scissors":
+         return "Rock smashes scissors! you win"
+     else:
+         return "Paper covers rock! you lose"
+    elif player == "paper":
+     if computer == "rock":
+         return "Paper covers rocks! you win"
+     else:
+         return "scissors cuts paper! you lose"
+    elif player == "scissors":
+     if computer == "paper":
+         return "scissors cuts paper! you win"
+     else:
+         return "Rock smashes scissors! you lose"
 
