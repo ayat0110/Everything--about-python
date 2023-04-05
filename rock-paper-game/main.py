fb3cb44f2743e63a030b804289bec80f2cd127d3
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-04-05 09:15:37 +0300

    Update main.py

--- rock-paper-game/main.py
+++ rock-paper-game/main.py
@@ -1,31 +1,32 @@
 import random
+
 def get_choices():
+  player_choice = input("Enter a choice (rock, paper, scissors): ")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  return choices
 
- player_choice = input("Enter a choice(rock,paper,scissors:")
- computer_choice = random.choice(options)
- options=["rock","paper","scissors"]
- #dictionaries in python are used to store data values in key value pairs
- choices={"player":player_choice,"computer":computer_choice}
- return choices
-
-def check_win(player,computer):
-    #f string is a simpler way to combine strings together
-    print(f"You chose {player}, computer chose {computer}" )
-    if player == computer:
-     return "It's a tie!"
-    elif player == "rock":
-     if computer == "scissors":
-         return "Rock smashes scissors! you win"
-     else:
-         return "Paper covers rock! you lose"
-    elif player == "paper":
-     if computer == "rock":
-         return "Paper covers rocks! you win"
-     else:
-         return "scissors cuts paper! you lose"
-    elif player == "scissors":
-     if computer == "paper":
-         return "scissors cuts paper! you win"
-     else:
-         return "Rock smashes scissors! you lose"
-
+def check_win(player, computer):
+  print(f"You chose {player}, computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
+  elif player == "rock":
+    if computer == "scissors":
+      return "Rock smashes scissors! You win!"
+    else:
+      return "Paper covers rock! You lose."
+  elif player == "paper":
+    if computer == "rock":
+      return "Paper covers rock! You win!"
+    else:
+      return "Scissors cuts paper! You lose."
+  elif player == "scissors":
+    if computer == "paper":
+      return "Scissors cuts paper! You win!"
+    else:
+      return "Rock smashes scissors! You lose."
+      
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
