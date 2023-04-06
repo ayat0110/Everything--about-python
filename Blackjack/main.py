ffa16e469e695df795db846b5fed46e27bbb9dac
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-04-06 12:21:02 +0300

    Update main.py

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -1,5 +1,8 @@
+import random
 cards = []
 suits = ["spades", "clubs", "heart", "diamonds"]
-ranks = ["A" ,"2" ,"3", "4", "5", "6", "7", "8" "9", "10", "J", "Q", "K"]
+ranks = ["A" ,"2" ,"3", "4", "5", "6", "7", "8" ,"9", "10", "J", "Q", "K"]
 for suit in suits:
-  print(suit)
+  for rank in ranks:
+     cards.append([suit, rank])
+print(cards)
